0e73fb885642f729f078b6f100ed6565bc3d1a69
Denisov-Egor <programming2028@gmail.com>
2026-05-17 15:01:19 +0300

    7

---
+++ 34.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n, f = 0;
+
+  cin >> n;
+
+  for (int i = 2; i <= n / 2; ++i)
+  {
+    if (n % i == 0)
+    {
+      f++;
+      break;
+    }
+  }
+  
+  if(f == 0)
+        cout<<n<<" - это простое число."<<endl;
+    else
+        cout<<n<<" не является простым числом."<<endl;
+}
