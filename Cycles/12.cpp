bf49035f300bb4eacb3a1fef667022fcb32bd651
Denisov-Egor <programming2028@gmail.com>
2026-05-25 20:50:10 +0300

    12

---
+++ Cycles/12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n; 
+  int fact = 1;
+  int i = 1;
+
+  cin >> n;
+
+  while (i <= n)
+  {
+    fact *= i;
+    i++;
+  }
+  cout << fact;
+}
